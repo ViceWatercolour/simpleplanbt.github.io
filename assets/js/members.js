6aafe6b27c5c1d38cfbe313d497edbc919111a4f
ExaBerries <exaberriesdev@gmail.com>
2014-04-27 09:02:00 -0700

    merge dis

--- assets/js/members.js
+++ assets/js/members.js
@@ -1,3 +1,5 @@
+var skinsURL = "http://s3.amazonaws.com/MinecraftSkins/";
+
 function showUsername(username, rank, about) {
   for (var i = 0; i < document.getElementsByTagName("li").length; i++) {
     var e = document.getElementsByTagName("li")[i];
@@ -8,8 +10,9 @@ function showUsername(username, rank, about) {
       e.className = "list-group-item active";
     }
   }
-  document.getElementById("member-photo").innerHTML = "<img src='images/member_photos/" + username + ".png'>";
+  document.getElementById("member-photo").innerHTML = "<img src='" + skinsURL + username + ".png' class='skin'>";
   document.getElementById("member-rank").innerHTML = "Rank: " + rank;
   document.getElementById("member-about-title").innerHTML = "About: " + username;
   document.getElementById("member-about").innerHTML = about;
+  renderMCSkins('skin', 3);
 }
