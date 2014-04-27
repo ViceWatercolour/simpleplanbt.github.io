6e4e4cc2d116d90ab16f4f805fd6318477a7201e
ExaBerries <exaberriesdev@gmail.com>
2014-04-27 08:11:52 -0700

    Finally seems to work

--- assets/js/members.js
+++ assets/js/members.js
@@ -8,8 +8,8 @@ function showUsername(username, rank, about) {
       e.style = "list-group-item active";
     }
   }
-  document.getElementById("member-photo").innerHTML = "<img src='/images/member_photos/" + username + ".png'>";
-  document.getElementById("member-rank").innerHTML = "Rank: " + username.sp;
+  document.getElementById("member-photo").innerHTML = "<img src='images/member_photos/" + username + ".png'>";
+  document.getElementById("member-rank").innerHTML = "Rank: " + rank;
   document.getElementById("member-about-title").innerHTML = "About: " + username;
   document.getElementById("member-about").innerHTML = about;
 }
