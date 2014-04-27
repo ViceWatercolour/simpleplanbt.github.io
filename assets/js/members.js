372d9706dd51fd6073451aec2c986d5f1fd4f194
ExaBerries <exaberriesdev@gmail.com>
2014-04-27 08:18:53 -0700

    Active now moves

--- assets/js/members.js
+++ assets/js/members.js
@@ -1,11 +1,11 @@
 function showUsername(username, rank, about) {
   for (var i = 0; i < document.getElementsByTagName("li").length; i++) {
     var e = document.getElementsByTagName("li")[i];
-    if (e.style == "list-group-item active") {
-      e.style = "list-group-item";
+    if (e.className == "list-group-item active") {
+      e.className = "list-group-item";
     }
-    if (e.nodeValue == "\w*" + username + "\w*") {
-      e.style = "list-group-item active";
+    if (e.innerHTML == username) {
+      e.className = "list-group-item active";
     }
   }
   document.getElementById("member-photo").innerHTML = "<img src='images/member_photos/" + username + ".png'>";
